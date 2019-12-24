d80e1f7ed800b1fb022d4b8b2e9d444a064584e2
Glitch (titanium-jumpsuit) <none>
2019-12-24 08:25:10 +0000

    ⛸🎑 Checkpoint ./server.js:9636892/167

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var bodyParser = require('body-parser');
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
@@ -12,13 +12,13 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
@@ -31,6 +31,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.post("/api/shorturl/new", function(req, res){
+  
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
